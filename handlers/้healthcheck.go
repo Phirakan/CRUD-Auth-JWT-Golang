ce812d22f7cff1a1ccc51012cd0f5db0e69eacce
Phirakan Khongphaet <peerakran03g@gmail.com>
2025-02-26 07:42:58 +0700

    Create healthcheck file
    create function for check connection

---
+++ handlers/้healthcheck.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"net/http"
+	"goapi/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+// CheckConnection 
+func CheckConnection(c *gin.Context) {
+	err := config.DB.Ping()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection failed"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Hello This is Your Go API Project"})
+}
