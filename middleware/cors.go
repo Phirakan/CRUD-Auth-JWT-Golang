f349a30753cb93316e2f4293cc88c81324a6d899
Phirakan Khongphaet <peerakran03g@gmail.com>
2025-05-20 05:03:18 +0700

    Edit CORS
    edit cors file for connect frontend

--- middleware/cors.go
+++ middleware/cors.go
@@ -5,11 +5,13 @@ import "github.com/gin-gonic/gin"
 // CORSMiddleware handles CORS setting
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE, PATCH")
+        c.Writer.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
 
+        // Handle preflight requests
         if c.Request.Method == "OPTIONS" {
             c.AbortWithStatus(204)
             return
